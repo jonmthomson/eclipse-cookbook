6be30f6661c1c4f383281bb27cb42cabaa7a4783
Jonathan Thomson <jonathan.mclean.thomson@gmail.com>
2015-02-07 09:32:59 +1300

    fixing cookbook dependency in eclipse cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'eclipse'
 maintainer       'Jonathan Thomson'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email ''
 license          'Apache 2.0'
 description      'Installs Eclipse on Windows using Chocolatey'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'windows'
-depends          'chocolatey'
+depends          'boxstarter'
